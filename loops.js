f8fba9caf16000e820cd9ec5d48005a949311c58
James Powell <nvrquitjp@gmail.com>
2019-04-22 15:16:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array){
 for (i = 0; i < 25; i++) {
 if(i===1){
-array.push("I am 1 strange loop.”);
+array.push("I am 1 strange loop.");
 } else {
-array.push("I am ${i} strange loop.”)
+array.push("I am ${i} strange loop.");
 }
 }
 return array;
